b5de24ef9456768c48c244c225c648f555c4c07f
Nataly <bzi92@bk.ru>
2021-05-18 20:24:51 +0300

    Add selenium and chromedriver

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,13 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
     }
 }
