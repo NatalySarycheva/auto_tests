651926960b3a20f45e0f740c824a12f38885cdee
Nataly <bzi92@bk.ru>
2021-05-18 21:31:26 +0300

    Add login

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,46 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class App
-{
+
+import java.util.concurrent.TimeUnit;
+
+public class App {
+
+    private static final String CRM_URL = "https://crm.geekbrains.space/user/login";
+
     public static void main( String[] args ) {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
         WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        driver.get(CRM_URL);
+
+        login(driver);
+
+        driver.quit();
+    }
+
+    private static void login(WebDriver driver) {
+        findElement(driver, "prependedInput", "ApplanaTest1");
+
+        findElement(driver, "prependedInput2", "Student2020!");
+
+        WebElement loginButton = driver.findElement(By.id("_submit"));
+        loginButton.click();
+
+        new WebDriverWait(driver, 3).until(ExpectedConditions.urlToBe("https://crm.geekbrains.space/"));
+    }
+
+    private static void findElement(WebDriver driver, String byId, String andSendKeys) {
+        WebElement loginField = driver.findElement(By.id(byId));
+        loginField.click();
+        loginField.sendKeys(andSendKeys);
     }
 }
